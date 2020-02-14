210f4bf514af32482c6d569bf116fbb949f212d6
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-02-14 15:20:00 +0100

    Add llvm-mingw plugin

---
+++ plugins/llvm-mingw/overrides.mk
@@ -0,0 +1,25 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+$(PLUGIN_HEADER)
+
+IS_LLVM := $(true)
+
+# Override sub-dependencies
+cc_DEPS := llvm
+llvm_DEPS := $(subst cc,llvm-mingw,$(llvm_DEPS))
+
+# Update mingw-w64 to 287a0d4
+# https://github.com/mingw-w64/mingw-w64/tarball/287a0d4d478e3575d082ba37ce4dcba86524fc9e
+# Keep-in sync with:
+# https://github.com/mstorsjo/llvm-mingw/blob/$(llvm-mingw_VERSION)/build-mingw-w64.sh#L21
+mingw-w64_VERSION  := 287a0d4
+mingw-w64_CHECKSUM := d057014aeb8ae68656e3a1de0d0e49fbe9e53a4b11d23daff53300539e6bad60
+mingw-w64_SUBDIR   := mingw-w64-mingw-w64-$(mingw-w64_VERSION)
+mingw-w64_FILE     := mingw-w64-mingw-w64-$(mingw-w64_VERSION).tar.gz
+mingw-w64_URL      := https://github.com/mingw-w64/mingw-w64/tarball/$(mingw-w64_VERSION)/$(mingw-w64_FILE)
+
+# libc++ uses Win32 threads to implement the internal
+# threading API, so we do not need to build pthreads.
+define pthreads_BUILD
+    $(info $(PKG) is not built when the llvm-mingw plugin is used)
+endef
