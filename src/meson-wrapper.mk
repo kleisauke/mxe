ad80f2aba86980f625e5fa1941cc38bb85a13dc7
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-02-14 15:20:00 +0100

    Add llvm-mingw plugin

--- src/meson-wrapper.mk
+++ src/meson-wrapper.mk
@@ -21,12 +21,10 @@ define $(PKG)_BUILD
         -DPREFIX=$(PREFIX) \
         -DTARGET=$(TARGET) \
         -DBUILD=$(BUILD) \
-        -DCPU_FAMILY=$(strip \
-             $(if $(findstring x86_64,$(TARGET)),x86_64,\
-             $(if $(findstring i686,$(TARGET)),x86))) \
-        -DCPU=$(strip \
-             $(if $(findstring x86_64,$(TARGET)),x86_64,\
-             $(if $(findstring i686,$(TARGET)),i686))) \
+        -DCPU_FAMILY=$(if $(findstring i686,$(PROCESSOR)),x86,$(PROCESSOR)) \
+        -DCPU=$(PROCESSOR) \
+        -DCC=$(if $(IS_LLVM),clang,gcc) \
+        -DCXX=$(if $(IS_LLVM),clang++,g++) \
         -DINPUT='$(PWD)/src/meson-wrapper/conf/mxe-crossfile.meson.in' \
         -DOUTPUT='$(PREFIX)/$(TARGET)/share/meson/mxe-crossfile.meson'
 
