104005fe50383baa9c01fca517e8df30a0b00620
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-03-03 15:00:00 +0100

    libffi: update test
    See:
    http://www.chiark.greenend.org.uk/doc/libffi-dev/html/Simple-Example.html#Simple-Example

--- src/libffi-test.c
+++ src/libffi-test.c
@@ -5,26 +5,30 @@
 #include <stdio.h>
 #include <ffi.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
     ffi_cif cif;
     ffi_type *args[1];
     void *values[1];
     char *s;
-    int rc;
+    ffi_arg rc;
 
-    (void)argc;
-    (void)argv;
-
+    /* Initialize the argument info vectors */
     args[0] = &ffi_type_pointer;
     values[0] = &s;
 
+    /* Initialize the cif */
     if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
-                           &ffi_type_uint, args) == FFI_OK)
+                           &ffi_type_sint, args) == FFI_OK)
     {
         s = "Hello World!";
         ffi_call(&cif, FFI_FN(puts), &rc, values);
-        s = "Goodbye!";
+        /* rc now holds the result of the call to puts */
+
+        /* values holds a pointer to the function's arg, so to
+           call puts() again all we need to do is change the
+           value of s */
+        s = "This is cool!";
         ffi_call(&cif, FFI_FN(puts), &rc, values);
     }
 
