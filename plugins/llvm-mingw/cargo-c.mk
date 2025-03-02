f2d61dbe19875c0f1d7b7c0549a5f8fb61a8f589
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-03-02 13:48:16 +0100

    Add Rust support in llvm-mingw plugin

---
+++ plugins/llvm-mingw/cargo-c.mk
@@ -0,0 +1,43 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := cargo-c
+$(PKG)_WEBSITE  := https://github.com/lu-zero/cargo-c
+$(PKG)_DESCR    := cargo applet to build and install C-ABI compatibile libraries
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 0.10.23
+$(PKG)_CHECKSUM := 17679af6c00a70ce1d70668023e993045539afdc7ab0ca1a081aa8ef6993a595
+$(PKG)_GH_CONF  := lu-zero/cargo-c/tags,v
+$(PKG)_TARGETS  := $(BUILD)
+$(PKG)_DEPS_$(BUILD) := $(BUILD)~rust
+
+define $(PKG)_PREPARE
+    MXE_ENABLE_NETWORK=1 $(WGET) -P '$(SOURCE_DIR)' \
+        'https://github.com/lu-zero/cargo-c/releases/download/v$($(PKG)_VERSION)/Cargo.lock'
+
+    # Ensure that the downloaded build dependencies of Cargo are
+    # stored in the build directory.
+    $(eval export CARGO_HOME := $(BUILD_DIR)/.cargo)
+
+    cd '$(SOURCE_DIR)' && MXE_ENABLE_NETWORK=1 cargo fetch \
+        --locked \
+        --target '$(shell rustc --print host-tuple)'
+endef
+
+define $(PKG)_BUILD_$(BUILD)
+    $($(PKG)_PREPARE)
+
+    # Disable LTO, panic strategy and optimization settings while
+    # we build cargo-c
+    $(eval unexport CARGO_PROFILE_RELEASE_LTO)
+    $(eval unexport CARGO_PROFILE_RELEASE_OPT_LEVEL)
+    $(eval unexport CARGO_PROFILE_RELEASE_PANIC)
+
+    cd '$(SOURCE_DIR)' && cargo build \
+        --release \
+        --frozen
+
+    cargo install \
+        --frozen \
+        --path='$(SOURCE_DIR)' \
+        --root='$(PREFIX)/$(BUILD)'
+endef
---
+++ plugins/llvm-mingw/rust.mk
@@ -0,0 +1,85 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := rust
+$(PKG)_WEBSITE  := https://www.rust-lang.org/
+$(PKG)_DESCR    := A systems programming language focused on safety, speed and concurrency.
+$(PKG)_IGNORE   :=
+# https://static.rust-lang.org/dist/2026-06-05/rustc-nightly-src.tar.xz.sha256
+$(PKG)_VERSION  := nightly
+$(PKG)_CHECKSUM := 89e9769b66c61eca90b885c852fbe34e952081c0e3332a7921eabfc7990d2afe
+$(PKG)_SUBDIR   := $(PKG)c-$($(PKG)_VERSION)-src
+$(PKG)_FILE     := $(PKG)c-$($(PKG)_VERSION)-src.tar.xz
+$(PKG)_URL      := https://static.rust-lang.org/dist/2026-06-05/$($(PKG)_FILE)
+$(PKG)_DEPS     := $(BUILD)~$(PKG)
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+
+$(PKG)_DEPS_$(BUILD) := $(BUILD)~llvm
+
+define $(PKG)_BUILD_$(BUILD)
+    # x86_64-pc-linux-gnu -> x86_64-unknown-linux-gnu
+    $(eval BUILD_RUST := $(firstword $(subst -, ,$(BUILD)))-unknown-linux-gnu)
+
+    # Disable LTO, panic strategy and optimization settings while
+    # we bootstrap Rust
+    $(eval unexport CARGO_PROFILE_RELEASE_LTO)
+    $(eval unexport CARGO_PROFILE_RELEASE_OPT_LEVEL)
+    $(eval unexport CARGO_PROFILE_RELEASE_PANIC)
+
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
+        --prefix='$(PREFIX)/$(BUILD)' \
+        --sysconfdir='etc' \
+        --release-channel=nightly \
+        --enable-vendor \
+        --enable-extended \
+        --tools=cargo,src \
+        --disable-lld \
+        --disable-docs \
+        --disable-codegen-tests \
+        --disable-manage-submodules \
+        --python='$(PYTHON3)' \
+        --llvm-root='$(PREFIX)/$(BUILD)' \
+        --set target.$(BUILD_RUST).cc='$(PREFIX)/$(BUILD)/bin/clang' \
+        --set target.$(BUILD_RUST).cxx='$(PREFIX)/$(BUILD)/bin/clang++' \
+        --set target.$(BUILD_RUST).linker='$(PREFIX)/$(BUILD)/bin/clang' \
+        --set target.$(BUILD_RUST).ar='$(PREFIX)/$(BUILD)/bin/llvm-ar' \
+        --set target.$(BUILD_RUST).ranlib='$(PREFIX)/$(BUILD)/bin/llvm-ranlib'
+
+    # Enable networking while we build Rust from source. Assumes
+    # that the Rust build is reproducible.
+    $(eval export MXE_ENABLE_NETWORK := 1)
+
+    # Ensure that the downloaded build dependencies of Cargo are
+    # stored in the build directory.
+    $(eval export CARGO_HOME := $(BUILD_DIR)/.cargo)
+
+    # Build and install Rust
+    # Note: we are only interested in the stage1 compiler
+    cd '$(BUILD_DIR)' && \
+        $(PYTHON3) $(SOURCE_DIR)/x.py install --stage 1 -j '$(JOBS)'
+endef
+
+define $(PKG)_BUILD
+    $(eval COMPILER_RT_LIB := $(shell $(TARGET)-clang --print-libgcc-file-name))
+    $(eval TARGET_RUST := $(PROCESSOR)-pc-windows-gnullvm)
+
+    # Install Cargo config
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/.cargo'
+    (echo '[unstable]'; \
+     echo 'build-std = ["std", "panic_abort"]'; \
+     echo 'build-std-features = ["optimize_for_size", "compiler-builtins-c"]'; \
+     echo 'trim-paths = true'; \
+     echo '[build]'; \
+     echo 'target = "$(TARGET_RUST)"'; \
+     echo '[env]'; \
+     echo 'CC_$(TARGET_RUST) = "$(TARGET)-clang"'; \
+     echo 'LLVM_COMPILER_RT_LIB = "$(COMPILER_RT_LIB)"'; \
+     echo '[target.$(TARGET_RUST)]'; \
+     echo 'ar = "$(PREFIX)/$(BUILD)/bin/llvm-ar"'; \
+     echo 'linker = "$(TARGET)-clang"'; \
+     echo 'rustflags = ['; \
+     $(if $(BUILD_STATIC), echo '    "-Ctarget-feature=+crt-static"$(comma)';) \
+     echo '    "-Zlocation-detail=none",'; \
+     echo '    "-Zfmt-debug=none"'; \
+     echo ']';) \
+             > '$(PREFIX)/$(TARGET)/.cargo/config.toml'
+endef
