4d027860f718647b07d9a37805e5e52c5447d306
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-04-05 10:54:27 +0200

    nasm: add mirror fallback

--- src/nasm.mk
+++ src/nasm.mk
@@ -9,6 +9,7 @@ $(PKG)_CHECKSUM := 87336eba53b4acfe917424ab5d500d2b0054d9f5148d35c2273ccf2cfb712
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://www.nasm.us/pub/$(PKG)/releasebuilds/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://fossies.org/linux/misc/$($(PKG)_FILE)
 $(PKG)_TARGETS  := $(BUILD)
 $(PKG)_DEPS     :=
 
