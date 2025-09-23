56c4b190c3f8552aab38571819f8c39287065bc8
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-09-23 18:29:42 +0200

    s|releases/tag|releases|g

--- src/sdl3.mk
+++ src/sdl3.mk
@@ -8,7 +8,7 @@ $(PKG)_VERSION  := 3.2.26
 $(PKG)_SUBDIR   := SDL3-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL3-$($(PKG)_VERSION).tar.gz
 $(PKG)_CHECKSUM := dad488474a51a0b01d547cd2834893d6299328d2e30f479a3564088b5476bae2
-$(PKG)_GH_CONF  := libsdl-org/SDL/releases/tag,release-,,
+$(PKG)_GH_CONF  := libsdl-org/SDL/releases,release-,,
 $(PKG)_DEPS     := cc libiconv libsamplerate
 
 define $(PKG)_BUILD
